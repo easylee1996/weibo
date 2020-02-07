35a102a1a86fb674135d5652d062ada78da76a48
easylee1996 <>
2020-02-07 23:29:42 +0800

    增加CSRF验证

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -7,13 +7,24 @@ use App\Models\User;
 
 class UsersController extends Controller
 {
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'name'     => 'required|max:50',
+            'email'    => 'required|email|unique:users|max:255',
+            'password' => 'required|confirmed|min:6',
+        ]);
+
+        return;
+    }
+
     public function create()
     {
         return view('users.create');
     }
 
     /**
-     * @param  User  $user 对应User模型资源路由{user}
+     * @param  User  $user  对应User模型资源路由{user}
      *
      * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
      */
