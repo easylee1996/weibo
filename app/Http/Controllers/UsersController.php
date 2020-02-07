41819774dc75fd04e0320b529cc0ed9c6820044f
easylee1996 <>
2020-02-07 23:13:13 +0800

    用户显示页面

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -3,11 +3,22 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
 
 class UsersController extends Controller
 {
     public function create()
     {
         return view('users.create');
+    }
+
+    /**
+     * @param  User  $user 对应User模型资源路由{user}
+     *
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
+    public function show(User $user)
+    {
+        return view('users.show', compact('user'));
     }
 }
