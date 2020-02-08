489befcb613caa24201014ca90ad4567ada5f358
easylee1996 <>
2020-02-08 15:10:53 +0800

    登陆成功后导航逻辑

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -12,6 +12,14 @@ class SessionsController extends Controller
         return view('sessions.create');
     }
 
+    public function destroy()
+    {
+        Auth::logout();
+        session()->flash('success', '您已成功退出！');
+
+        return redirect('login');
+    }
+
     public function store(Request $request)
     {
         $credentials = $this->validate($request, [
@@ -21,16 +29,20 @@ class SessionsController extends Controller
 
         if (Auth::attempt($credentials)) {
             session()->flash('success', '欢迎回来！');
+
             return redirect()->route('users.show', [Auth::user()]);
         } else {
             session()->flash('danger', '很抱歉，您的邮箱和密码不匹配');
+
             return redirect()->back()->withInput();
         }
 
         // 注册后自动登陆
         Auth::login($user);
         session()->flash('success', '欢迎，您将在这里开启一段新的旅程~');
+
         return redirect()->route('users.show', [$user]);
+
         return;
     }
 }
